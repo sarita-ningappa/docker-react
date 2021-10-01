070e56a98d122f19ed995a9b0de2a5ebc5966844
saritahiral <hiralsarita@gmail.com>
2021-10-01 14:35:32 +0530

    feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          its a miracle
+          changed on feature branch
         </p>
         <a
           className="App-link"
